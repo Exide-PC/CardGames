6c2f6253882f655523c7300156082bb0d5a92812
Exide <ig.roman95@mail.ru>
2019-08-05 23:26:09 +0300

    Added semantic names for cards

--- CardGames.Core/Presenters/DurakPresenter.cs
+++ CardGames.Core/Presenters/DurakPresenter.cs
@@ -51,8 +51,7 @@ namespace CardGames.Core.Presenters
         {
             return new PlayerState
             {
-                Hand = _game.Players.First(p => p.Id == playerId).Hand,
-                Trump = _game.Trump,
+                Trump = _game.Trump.ToString(),
                 GameState = _game.State.ToString(),
                 DeckCount = _game.Deck.Count,
                 IsAttack = _game.IsAttack,
@@ -60,22 +59,56 @@ namespace CardGames.Core.Presenters
                 AttackerId = _game.Players[_game.AttackerIndex].Id,
                 CurrentPlayerId = _game.CurrentPlayer.Id,
                 Players = _game.Players.Select(p => new {Id = p.Id, Name = _nameMap[p.Id]}),
-                Attacks = _game.Attacks.Select(a => (attacker: a.Attacker, defender: a.Defender))
+                Hand = _game.Players.First(p => p.Id == playerId).Hand.Select(c => new NamedCard(c)),
+                Attacks = _game.Attacks.Select(a => (new NamedCard(a.Attacker), new NamedCard(a.Defender)))
             };
         }
 
         public class PlayerState
         {
-            public IEnumerable<Card> Hand { get; set; }
-            public CardSuit Trump { get; internal set; }
+            public string Trump { get; set; }
             public string GameState { get; set; }
             public int DeckCount { get; set; }
             public bool IsAttack { get; set; }
-            public int DefenderId { get; internal set; }
+            public int DefenderId { get; set; }
             public int AttackerId { get; set; }
             public int CurrentPlayerId { get; set; }
             public IEnumerable<object> Players { get; set; }
-            public IEnumerable<(Card attacker, Card defender)> Attacks { get; set; }
+            public IEnumerable<NamedCard> Hand { get; set; }
+            public IEnumerable<(NamedCard attacker, NamedCard defender)> Attacks { get; set; }
+        }
+
+        public class NamedCard
+        {
+            public string Suit { get; }
+            public int Value { get; }
+            public string Name { get; }
+
+            public NamedCard(Card card)
+            {
+                this.Suit = card.Suit.ToString();
+                this.Value = card.Value;
+                this.Name = GetName(card.Value);
+            }
+
+            private string GetName(int value)
+            {
+                switch (value)
+                {
+                    case int v when v <= 4: // 0,1,2,3,4 => 6,7,8,9,10
+                        return (v + 6).ToString();
+                    case 5:
+                        return "Jack";
+                    case 6:
+                        return "Queen";
+                    case 7:
+                        return "King";
+                    case 8:
+                        return "Ace";
+                    default:
+                        throw new Exception($"No semantic name for value {value}");
+                }
+            }
         }
     }
 }
