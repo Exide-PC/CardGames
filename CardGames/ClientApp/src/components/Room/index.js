a88c08094d162f4fc9fa040d8b40c8497644eefd
Nikita <nikitsidenk@mail.ru>
2019-08-13 00:00:19 +0300

    show users names while hovering / ui fix

--- CardGames/ClientApp/src/components/Room/index.js
+++ CardGames/ClientApp/src/components/Room/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 const Room = ({ players, number, hasSlots, onClick }) => {
 
+  const [show, setShow] = useState(false);
+
+  const onMouseOver = () => setShow(true);
+  const onMouseOut = () => setShow(false);
+
   return (
     <div className='room-container' onClick={onClick}>
-      <div className='room'>
+      <div className='room' onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
         <div className='room__background' />
         <h2 className='room__number'>Комната № {number}</h2>
-        <p className='room__players'>Игрков: {players.length}</p>
+        {!show && <p className='room__players'>Игрков: {players.length}</p>}
+        {show && <p className='room__players'>{players.join(' | ')}</p>}
         {hasSlots ? 
           <p>Есть места</p>:
           <p className='room__full' >Мест нет</p>
