86d3afd667d5d4e3b62da318ab937dbf25dc4453
Nikita Sidenko <nikitsidenk@mail.ru>
2019-08-22 13:12:17 +0300

    refact

--- CardGames/ClientApp/postcss.config.js
+++ CardGames/ClientApp/postcss.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('postcss-normalize')({
+      forceImport: true
+    }),
+    require('cssnano')({
+      preset: 'default'
+    }),
     require('postcss-import'),
     require('postcss-preset-env')({
       stage: 0,
+      autoprefixer: { grid: true },
+      features: {
+        'custom-media-queries': true,
+        'nesting-rules': true
+      },
       importFrom: './src/base.css'
-    }),
-    require('postcss-nested')
+    })
   ]
 };
