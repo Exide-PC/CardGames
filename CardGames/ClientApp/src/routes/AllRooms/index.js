b1331f1e63aa37caeaa70335b18c9fced42329b6
Nikita <nikitsidenk@mail.ru>
2019-08-26 23:54:40 +0300

    fix

--- CardGames/ClientApp/src/routes/AllRooms/index.js
+++ CardGames/ClientApp/src/routes/AllRooms/index.js
@@ -6,14 +6,19 @@ import useApi from '../../components/Hooks/useApi';
 import Loader from '../../components/Loader';
 import Api from './api';
 
+import cx from 'classnames';
+
 import './style.css';
 
 const AllRooms = () => {
   const [activeModal, setActiveModal] = useState(false);
   const [activeCreateModal, setActiveCreateModal] = useState(false);
   const [rooms, loading] = useApi('api/lobby/list', 2000);
+  const [scrollHeight, setScrollHeight] = useState(0);
 
-  console.log(rooms);
+  useEffect(() => {
+    setScrollHeight(document.documentElement.scrollHeight);
+  }, [document.documentElement.scrollHeight]);
 
   // Api.createRoom({ nick: 'name', room: 'room' })
 
@@ -27,8 +32,13 @@ const AllRooms = () => {
     setActiveCreateModal(false);
   };
 
+  const containerClassName = cx('bg-rooms', {
+    'bg-rooms--full-height':
+      scrollHeight === document.documentElement.clientHeight
+  });
+
   return (
-    <div className="bg-rooms">
+    <div className={containerClassName}>
       <div className="container">
         <Button text="Создать игру" onClick={openCreate} />
         {!loading && !rooms && <h1 className="no-games">Нет доступных игр</h1>}
